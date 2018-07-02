b974b6ccdc3882260a10cf0d8b8662acc85d3e78
adolha <adrian.dolha@endava.com>
2018-07-02 16:36:34 +0300

    Minor changes

--- templates/connector/create_lambda_package.ps1
+++ templates/connector/create_lambda_package.ps1
@@ -1,5 +1,5 @@
-cd c:\apps\metadata_manager
+cd c:\apps\cloud_demo
 rm -Force -Recurse lambda_package
 New-Item -Type Directory lambda_package
 Copy-Item -Destination lambda_package -Path connector -Recurse -v
-Compress-Archive lambda_package\* -DestinationPath templates\lambda_package.zip -v -Force
+Compress-Archive lambda_package\* -DestinationPath lambda_package.zip -v -Force
