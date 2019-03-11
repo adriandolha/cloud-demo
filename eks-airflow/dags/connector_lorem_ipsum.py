93291c7d9cb467fd8e3839f663cf39c9383c16da
adriandolha <adriandolha@yahoo.com>
2019-03-11 20:16:24 +0200

    Airflow sample kubernetes DAG. Read secret from variable.

---
+++ eks-airflow/dags/connector_lorem_ipsum.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import Variable
+
+redis_password = Variable.get("redis-password")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': True,
+    'start_date': datetime.utcnow()
+}
+dag = DAG('connector-lorem-ipsum', schedule_interval='*/10 * * * *', default_args=default_args)
+
+KubernetesPodOperator(namespace='airflow',
+                      image="103050589342.dkr.ecr.eu-central-1.amazonaws.com/connector-lorem-ipsum:1.9",
+                      env_vars={'CELERY_BROKER_URL': 'redis://:airflow@airflow-redis-master:6379/0'},
+                      arguments=["action=create-job", f'no_of_pages=100', 'job_id=book_1'],
+                      name="create-job",
+                      task_id="create-job",
+                      is_delete_operator_pod=True,
+                      hostnetwork=False,
+                      in_cluster=True,
+                      dag=dag
+                      )
+KubernetesPodOperator(namespace='airflow',
+                      image="103050589342.dkr.ecr.eu-central-1.amazonaws.com/connector-lorem-ipsum:1.15",
+                      env_vars={'CELERY_BROKER_URL': 'redis://:airflow@airflow-redis-master:6379/0',
+                                'REDIS_HOST': 'airflow-redis-master',
+                                'REDIS_PASSWORD': redis_password},
+                      arguments=["action=download-book", 'job_id=book_1'],
+                      name="download-book",
+                      task_id="download-book",
+                      is_delete_operator_pod=True,
+                      hostnetwork=False,
+                      in_cluster=True,
+                      dag=dag
+                      )
