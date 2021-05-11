9eb029390a482f18a80daefa15f4ad9c570bd95d
adriandolha <adriandolha@yahoo.com>
2021-05-11 12:16:41 +0300

    [lorem-ipsum] - CI/cd with Tekton/ArgoCD

---
+++ lorem-ipsum-eks/infrastructure/cicd/local/tekton.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# https://docs.ventuscloud.eu/docs/tutorials/tekton-pipelines
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+brew install istioctl
+cat << EOF > ./istio-minimal-operator.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  values:
+    global:
+      proxy:
+        autoInject: disabled
+      useMCP: false
+      # The third-party-jwt is not enabled on all k8s.
+      # See: https://istio.io/docs/ops/best-practices/security/#configure-third-party-service-account-tokens
+      jwtPolicy: first-party-jwt
+
+  addonComponents:
+    pilot:
+      enabled: true
+
+  components:
+    ingressGateways:
+      - name: istio-ingressgateway
+        enabled: true
+EOF
+istioctl install -f istio-minimal-operator.yaml
+
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.0/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.0/serving-core.yaml
+kubectl apply -f https://github.com/knative/net-kourier/releases/download/v0.22.0/kourier.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/v0.22.0/eventing-crds.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/v0.22.0/eventing-core.yaml
+kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.10.1/tekton-webhooks-extension-release.yaml -n tekton-pipelines
--- lorem-ipsum-eks/infrastructure/monitoring/local/deploy.sh
+++ lorem-ipsum-eks/infrastructure/monitoring/local/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add stable https://charts.helm.sh/stable
 kubectl delete ns monitoring
 kubectl apply -f monitoring-namespace.yaml
 kubectl apply -f prometheus-config.yaml
 kubectl apply -f prometheus-deployment.yaml
 kubectl apply -f prometheus-service.yaml
 helm del --purge grafana
-helm install stable/grafana -f grafana-values.yaml -n grafana
+helm install grafana stable/grafana -f grafana-values.yaml -n grafana
