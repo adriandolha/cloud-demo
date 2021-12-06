39bdec2bbf84b8d0ce35113af7b3a6ca752e3498
adriandolha <adriandolha@yahoo.com>
2021-12-06 10:51:28 +0200

    [lorem-ipsum] - CI/cd with Tekton/ArgoCD. Include terraform in the pipeline for Infrastructure provisioning. Handling secrets.

--- lorem-ipsum-eks/infrastructure/local/blackbox-exporter.tf
+++ lorem-ipsum-eks/infrastructure/local/blackbox-exporter.tf
@@ -1,6 +1,9 @@
 resource "helm_release" "prometheus-blackbox-exporter" {
-  chart      = "prometheus-blackbox-exporter"
-  name       = "prometheus-blackbox-exporter"
-  namespace  = kubernetes_namespace.ns.metadata[0].name
+  chart = "prometheus-blackbox-exporter"
+  name = "prometheus-blackbox-exporter"
+  namespace = kubernetes_namespace.ns.metadata[0].name
   repository = "https://prometheus-community.github.io/helm-charts"
+  depends_on = [
+    helm_release.grafana,
+    helm_release.prometheus]
 }
