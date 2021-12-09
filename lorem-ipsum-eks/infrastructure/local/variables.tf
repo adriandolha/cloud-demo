30d21e74c6ea270446a16655f853c4da8ab4249f
adriandolha <adriandolha@yahoo.com>
2021-12-09 07:24:19 +0200

    [lorem-ipsum] - CI/cd with Tekton/ArgoCD. Include terraform in the pipeline for Infrastructure provisioning. Putting all together.

--- lorem-ipsum-eks/infrastructure/local/variables.tf
+++ lorem-ipsum-eks/infrastructure/local/variables.tf
@@ -3,6 +3,13 @@ variable "env" {
   description = "Env to deploy to. It will translate into a namespace."
   default = "dev"
 }
+
+variable "namespace" {
+  type = string
+  description = "Namespace."
+  default = "dev"
+}
+
 variable "kube_config_file" {
   type = string
   default = "~/.kube/config"
